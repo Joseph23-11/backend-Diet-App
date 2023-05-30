dacd1e07c8e31046483d4bfd871abb143cbb7e9a
Joseph23-11 <joseph.simarmata2311@mail.ugm.ac.id>
2023-05-30 22:05:45 +0700

    make models of breakfast, lunch, dinner and snack

--- database/migrations/2023_05_30_220015_create_snacks_table.php
+++ database/migrations/2023_05_30_220015_create_snacks_table.php
@@ -8,19 +8,33 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('snacks', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('food_id');
+            $table->float('porsi_makanan');
+            $table->float('kalori');
+            $table->float('protein');
+            $table->float('lemak');
+            $table->float('karbohidrat');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('food_id')->references('id')->on('food')->onDelete('cascade');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('snacks');
     }
