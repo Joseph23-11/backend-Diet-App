68357950c5518da86ee1ecfdda5c15ea25f4078a
Joseph23-11 <joseph.simarmata2311@mail.ugm.ac.id>
2023-07-10 15:09:08 +0700

    add regresi linear

--- app/Http/Controllers/PrediksiController.php
+++ app/Http/Controllers/PrediksiController.php
@@ -72,7 +72,7 @@ class PrediksiController extends Controller
         for ($index = 0; $index < $maxX; $index++) {
             $x = $index + 1;
             $y = $c[0] + $c[1] * $x;
-            $yFormatted = number_format($y, 2);
+            $yFormatted = number_format($y, 0);
             $regressionLine[] = [
                 'hari' => date('Y-m-d', strtotime($perubahanBerats[0]->created_at . ' + ' . $index . ' days')),
                 'berat' => $yFormatted
@@ -81,7 +81,7 @@ class PrediksiController extends Controller
 
         // Menghitung prediksi berat sekarang
         $prediksiBeratSekarang = $c[0] + $c[1] * ($perubahanBerats->count() + 1);
-        $prediksiBeratSekarangFormatted = number_format($prediksiBeratSekarang, 2);
+        $prediksiBeratSekarangFormatted = number_format($prediksiBeratSekarang, 0);
 
         // Menambahkan tanggal prediksi berat sekarang
         $tanggalPrediksi = date('Y-m-d', strtotime($perubahanBerats[1]->created_at . ' + ' . $perubahanBerats->count() . ' days'));
@@ -95,14 +95,16 @@ class PrediksiController extends Controller
             $hariKeXRounded = ceil($hariKeX) + 1;
         }
 
-        return response()->json([
+        $response = [
             'prediksi_berat_sekarang' => [
                 'tanggal' => $tanggalPrediksi,
                 'berat' => $prediksiBeratSekarangFormatted
             ],
             'hari_target' => $hariKeXRounded,
             'regression_line' => $regressionLine,
-        ]);
+        ];
+
+        return response()->json($response);
     }
 
     // Fungsi untuk mengalikan matriks dengan vektor
